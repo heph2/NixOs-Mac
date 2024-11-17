62b4d38d9938191f9edc6e4b1c7bd2696abe11b7
heph <srht@mrkeebs.eu>
2024-11-17 09:23:46 +0100

    feat(nix): update flake

--- flake.nix
+++ flake.nix
@@ -2,20 +2,23 @@
   description = "Marco's darwin system";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-23.11-darwin";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-24.05-darwin";
     nixpkgs-unstable.url = github:NixOS/nixpkgs/nixpkgs-unstable;
-    darwin.url = "github:lnl7/nix-darwin/master";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    darwin = {
+      url = "github:lnl7/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     emacs.url = "github:cmacrae/emacs";
-    emacs-overlay.url = "github:nix-community/emacs-overlay/db47b2483942771a725cf10e7cd3b1ec562750b7";
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    emacs-overlay = {
+      url = "github:nix-community/emacs-overlay/db47b2483942771a725cf10e7cd3b1ec562750b7";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nur.url = "github:nix-community/NUR";
     devenv.url = "github:cachix/devenv";
-#    nixvim = {
-#      url = "github:nix-community/nixvim";
-#      inputs.nixpkgs.follows = "nixpkgs-unstable";
-#    };
     cachix-deploy-flake.url = "github:cachix/cachix-deploy-flake";
     cachix-deploy-flake.inputs.darwin.follows = "darwin";
     flake-parts.url = "github:hercules-ci/flake-parts";
@@ -27,7 +30,6 @@
         ./flake/aron/default.nix
       ];
       systems = [
-        # systems for which you want to build the `perSystem` attributes
         "x86_64-linux"
         "aarch64-darwin"
       ];
